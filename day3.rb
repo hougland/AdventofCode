99afb86b862ddd45c3f980b36ab7d2d9382979fd
Ricky Hougland <houglande@gmail.com>
2015-12-19 21:12:21 -0800

    day 3 part 2 complete - though not DRYly

--- day3.rb
+++ day3.rb
@@ -4,20 +4,37 @@ def houses(instructions)
   array = instructions[0][0].split(//)
 
   visited = [[0,0]]
-  x = 0
-  y = 0
+  s_x = 0
+  s_y = 0
 
-  array.each do |direction|
-    if direction == "^"
-      y += 1
-    elsif direction == "v"
-      y -= 1
-    elsif direction == ">"
-      x += 1
-    elsif direction == "<"
-      x -= 1
+  r_x = 0
+  r_y = 0
+
+  array.each_with_index do |direction, index|
+    if index % 2 == 0 # this is Santa
+      if direction == "^"
+        s_y += 1
+      elsif direction == "v"
+        s_y -= 1
+      elsif direction == ">"
+        s_x += 1
+      elsif direction == "<"
+        s_x -= 1
+      end
+    else # this is Robo-Santa
+      if direction == "^"
+        r_y += 1
+      elsif direction == "v"
+        r_y -= 1
+      elsif direction == ">"
+        r_x += 1
+      elsif direction == "<"
+        r_x -= 1
+      end
     end
-    visited.push([x, y])
+
+    visited.push([s_x, s_y])
+    visited.push([r_x, r_y])
   end
 
   return visited.uniq.length
