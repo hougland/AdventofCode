edd2d6d4fcc91234b269312b83329bb59b10f64c
Ricky Hougland <houglande@gmail.com>
2015-12-31 23:29:02 -0500

    day 4 part 2

--- day4.rb
+++ day4.rb
@@ -1,12 +1,20 @@
 require 'digest'
 require 'digest/bubblebabble'
 
-md5 = Digest::MD5.hexdigest("bgvyzdsv")
-num = 1
+def find_md5(num_zeros)
+  md5 = Digest::MD5.hexdigest("bgvyzdsv")
+  num = 1
 
-while md5[0...5] != "00000"
-  num += 1
-  md5 = Digest::MD5.hexdigest("bgvyzdsv#{num}")
+  while md5[0...num_zeros] != "0" * num_zeros
+    num += 1
+    md5 = Digest::MD5.hexdigest("bgvyzdsv#{num}")
+  end
+
+  puts "The md5 that begins with #{num_zeros} zeros is #{num}."
 end
 
-puts num
+find_md5(5)
+find_md5(6)
+
+# 5 zeros: 254575
+# 6 zeros: 1038736
